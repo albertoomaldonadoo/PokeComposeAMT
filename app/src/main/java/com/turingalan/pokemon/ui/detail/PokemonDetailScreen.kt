80f7b180faca5e894dbe0307dda7ed77d20fae02
Alberto Maldonado Triana <amaltri0308@cpifpalanturing.es>
2025-10-30 11:13:19 +0100

    commit de clase

--- app/src/main/java/com/turingalan/pokemon/ui/detail/PokemonDetailScreen.kt
+++ app/src/main/java/com/turingalan/pokemon/ui/detail/PokemonDetailScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -46,6 +47,9 @@ fun PokemonDetailContent(
             painter = painterResource(artworkId),
             contentDescription = name,
             contentScale = ContentScale.Crop
+        )
+        Text(
+            "Nombre del pokemon: $name"
         )
     }
 }
